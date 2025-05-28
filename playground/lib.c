284b38582796c99981cd84c4fc45ec6843133ef6
Yuzunoha <studysteadystandby@gmail.com>
2025-05-28 21:45:09 +0900

    いったんコミット

--- playground/lib.c
+++ playground/lib.c
@@ -1,3 +1,40 @@
+#include <stdio.h>
+#include <fcntl.h>
+#include <linux/input.h>
+#include <unistd.h>
+#include <string.h>
+
+void print_keyboard_device() {
+  for (int i = 0; i < 10; i++) { // 例として0から9までのeventファイルをチェック
+    char path[20];
+    snprintf(path, sizeof(path), "/dev/input/event%d", i);
+    
+    int fd = open(path, O_RDONLY);
+    if (fd < 0) {
+      continue; // 開けなかった場合はスキップ
+    }
+
+    struct input_id id;
+    if (ioctl(fd, EVIOCGID, &id) == -1) {
+      perror("ioctl");
+      close(fd);
+      continue;
+    }
+
+    // デバイスタイプの確認
+    if (id.bustype == BUS_USB || id.bustype == BUS_HOST) {
+      struct input_event ev;
+      if (read(fd, &ev, sizeof(ev)) > 0) {
+        if (ev.type == EV_KEY) {
+          printf("%s is a keyboard device\n", path);
+        }
+      }
+    }
+
+    close(fd);
+  }
+}
+
 int add(int x, int y) {
   return x + y;
 }
--- playground/main.c
+++ playground/main.c
@@ -2,14 +2,11 @@
 
 // 他のファイルで定義された関数の宣言
 int add(int a, int b);
+void print_keyboard_device();
 
 // 実行コマンド
 // gcc -o main *.c; chmod +x main; ./main; rm ./main;
 int main() {
-  printf("こんにちは!!\n");
-  int result = add(5, 3);
-  printf("Result: %d\n", result);
-  result = add(5, -4);
-  printf("Result: %d\n", result);
+  print_keyboard_device();
   return 0;
 }
