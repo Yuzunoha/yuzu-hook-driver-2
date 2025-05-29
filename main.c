f08278a39d3e6c34455b1dbe0164283af7780feb
Yuzunoha <studysteadystandby@gmail.com>
2025-05-29 20:12:12 +0900

    再出力

--- main.c
+++ main.c
@@ -1,92 +1,132 @@
+// remap_i_to_o.c
+
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <fcntl.h>
 #include <unistd.h>
 #include <linux/uinput.h>
 #include <linux/input.h>
-#include <string.h>
 #include <errno.h>
-#include <sys/ioctl.h>
+#include <signal.h>
 
-#define DEVICE "/dev/input/event4" // キーボード入力元
+#define DEVICE "/dev/input/event4"
+
+int uinput_fd = -1;
+int input_fd = -1;
+
+void cleanup(int signo)
+{
+  if (uinput_fd >= 0)
+  {
+    ioctl(uinput_fd, UI_DEV_DESTROY);
+    close(uinput_fd);
+  }
+
+  if (input_fd >= 0)
+  {
+    ioctl(input_fd, EVIOCGRAB, 0); // グラブ解除
+    close(input_fd);
+  }
+
+  printf("\n[INFO] Exit cleanly\n");
+  exit(0);
+}
 
 int setup_uinput_device()
 {
-  int uinput_fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
+  struct uinput_user_dev uidev;
+
+  uinput_fd = open("/dev/uinput", O_WRONLY | O_NONBLOCK);
   if (uinput_fd < 0)
   {
     perror("open /dev/uinput");
     exit(1);
   }
 
-  // 対応するイベント種別とキーをセット
   ioctl(uinput_fd, UI_SET_EVBIT, EV_KEY);
   ioctl(uinput_fd, UI_SET_EVBIT, EV_SYN);
+  ioctl(uinput_fd, UI_SET_KEYBIT, KEY_O);
 
-  for (int i = 0; i < 256; i++)
-  {
-    ioctl(uinput_fd, UI_SET_KEYBIT, i);
-  }
-
-  struct uinput_user_dev uidev = {};
-  snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "remap_keyboard");
+  memset(&uidev, 0, sizeof(uidev));
+  snprintf(uidev.name, UINPUT_MAX_NAME_SIZE, "i_to_o_remap_dev");
   uidev.id.bustype = BUS_USB;
-  uidev.id.vendor = 0x1234;
-  uidev.id.product = 0x5678;
+  uidev.id.vendor = 0x1;
+  uidev.id.product = 0x1;
   uidev.id.version = 1;
 
   write(uinput_fd, &uidev, sizeof(uidev));
-  ioctl(uinput_fd, UI_DEV_CREATE);
+  if (ioctl(uinput_fd, UI_DEV_CREATE) < 0)
+  {
+    perror("UI_DEV_CREATE");
+    exit(1);
+  }
 
-  sleep(1); // デバイスが有効になるまで待つ
-
+  sleep(1); // デバイス準備待ち
   return uinput_fd;
 }
 
-void emit(int fd, int type, int code, int value)
+void send_key_event(int fd, int keycode, int value)
 {
   struct input_event ev;
+
+  memset(&ev, 0, sizeof(ev));
   gettimeofday(&ev.time, NULL);
-  ev.type = type;
-  ev.code = code;
+  ev.type = EV_KEY;
+  ev.code = keycode;
   ev.value = value;
+  write(fd, &ev, sizeof(ev));
+
+  memset(&ev, 0, sizeof(ev));
+  gettimeofday(&ev.time, NULL);
+  ev.type = EV_SYN;
+  ev.code = SYN_REPORT;
+  ev.value = 0;
   write(fd, &ev, sizeof(ev));
 }
 
 int main()
 {
-  int input_fd = open(DEVICE, O_RDONLY);
+  struct input_event ev;
+
+  // SIGINTなどを受けたらクリーンアップする
+  signal(SIGINT, cleanup);
+  signal(SIGTERM, cleanup);
+
+  // 入力デバイスオープン
+  input_fd = open(DEVICE, O_RDONLY);
   if (input_fd < 0)
   {
     perror("open input device");
     return 1;
   }
 
-  int uinput_fd = setup_uinput_device();
+  // 入力デバイスを「グラブ」して他のプロセスに渡さない
+  if (ioctl(input_fd, EVIOCGRAB, 1) < 0)
+  {
+    perror("EVIOCGRAB");
+    return 1;
+  }
 
-  struct input_event ev;
+  // 仮想出力デバイス作成
+  setup_uinput_device();
+
+  printf("[INFO] Remapping 'i' to 'o'...\n");
+
+  // イベントループ
   while (1)
   {
-    ssize_t n = read(input_fd, &ev, sizeof(ev));
-    if (n != sizeof(ev))
+    if (read(input_fd, &ev, sizeof(ev)) != sizeof(ev))
+    {
       continue;
+    }
 
     if (ev.type == EV_KEY && ev.code == KEY_I)
     {
-      ev.code = KEY_O;
-    }
-
-    emit(uinput_fd, ev.type, ev.code, ev.value);
-
-    if (ev.type == EV_KEY || ev.type == EV_SYN)
-    {
-      emit(uinput_fd, EV_SYN, SYN_REPORT, 0);
+      // 'i' を 'o' に置き換える
+      send_key_event(uinput_fd, KEY_O, ev.value);
     }
   }
 
-  // 通常ここに来ないが、終了処理を書くなら以下
-  ioctl(uinput_fd, UI_DEV_DESTROY);
-  close(uinput_fd);
-  close(input_fd);
   return 0;
 }
