bb6e2e9d9f4b8074c7e4f54cf9111453ff78b903
Yuzunoha <studysteadystandby@gmail.com>
2025-05-29 19:36:16 +0900

    バーっと書いたけど動いた

--- test2/is_key.c
+++ test2/is_key.c
@@ -6,65 +6,72 @@
 #include <string.h>
 #include <errno.h>
 
-#define DEVICE_PATH "/dev/input/event4"
+#define DEVICE_PATH "/dev/input/event0"
 #define BIT_LEN (KEY_MAX / 8 + 1)
 
-int this_device_path_is_a_keyboard(char *device_path)
+/**
+ * openされたfdを使って判定だけする。closeは呼び出し元でやる
+ */
+int is_a_keyboard_without_closing_the_device(int fd)
 {
-  int fd;
   unsigned char evbit[EV_MAX / 8 + 1];
   unsigned char keybit[BIT_LEN];
-
   memset(evbit, 0, sizeof(evbit));
   memset(keybit, 0, sizeof(keybit));
 
-  fd = open(DEVICE_PATH, O_RDONLY);
-  if (fd < 0)
-  {
-    perror("open");
-    return 1;
-  }
-
-  // イベントタイプ（EV_KEY など）を取得
+  // イベントタイプ（EV_KEY など）を取得する
   if (ioctl(fd, EVIOCGBIT(0, sizeof(evbit)), evbit) < 0)
   {
-    perror("ioctl EVIOCGBIT ev");
-    close(fd);
-    return 1;
-  }
-
-  // キーイベントがサポートされているか確認
-  if (!(evbit[EV_KEY / 8] & (1 << (EV_KEY % 8))))
-  {
-    printf("This is NOT a keyboard: no EV_KEY support.\n");
-    close(fd);
+    // 取得できなかった
     return 0;
   }
 
-  // サポートされているキーの一覧を取得
+  // キーイベントがサポートされているか確認する
+  if (!(evbit[EV_KEY / 8] & (1 << (EV_KEY % 8))))
+  {
+    // サポートされていなかった
+    return 0;
+  }
+
+  // サポートされているキーの一覧を取得する
   if (ioctl(fd, EVIOCGBIT(EV_KEY, sizeof(keybit)), keybit) < 0)
   {
-    perror("ioctl EVIOCGBIT key");
-    close(fd);
+    // 取得できなかった
+    return 0;
+  }
+
+  // 'A' キーなど、キーボードらしいキーがあるかを確認する
+  if (keybit[KEY_A / 8] & (1 << (KEY_A % 8)))
+  {
+    // あった！キーボードであるとみなす
     return 1;
   }
 
-  // 'A' キーなど、キーボードらしいキーがあるかを確認
-  if (keybit[KEY_A / 8] & (1 << (KEY_A % 8)))
-  {
-    printf("This is a keyboard device.\n");
-  }
-  else
-  {
-    printf("This is NOT a keyboard: no KEY_A.\n");
-  }
-
-  close(fd);
+  // ここまで来たらキーボードではないとみなす
   return 0;
+}
+
+int this_device_path_is_a_keyboard(char *device_path)
+{
+  // 指定されたフルパスのデバイスを開く
+  int fd = open(DEVICE_PATH, O_RDONLY);
+  if (fd < 0)
+  {
+    // 開けなかった
+    return 0;
+  }
+  // 判定だけする
+  int result = is_a_keyboard_without_closing_the_device(fd);
+  // fdを必ず閉じる
+  close(fd);
+  // 結果を返却する
+  return result;
 }
 
 int main()
 {
-  this_device_path_is_keyboard(DEVICE_PATH);
+  int result = this_device_path_is_a_keyboard(DEVICE_PATH);
+  printf("%sはキーボードで%s\n", DEVICE_PATH, (result ? "ある" : "ない"));
+
   return 0;
 }
