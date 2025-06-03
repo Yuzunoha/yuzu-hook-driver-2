481da602df1e7b6a053ece10d6ec0d52df4e2077
Yuzunoha <studysteadystandby@gmail.com>
2025-06-03 18:45:35 +0900

    思惑通り

--- simple/event_loop.c
+++ simple/event_loop.c
@@ -1,20 +1,35 @@
 #include <linux/input.h>
 #include <stdio.h>
 
+// Linuxにおけるvalueが表すキーの状態 (0,1,2) = (離された,押された,押し続けている)
+int g_value_muhenkan;
+
+// 無変換押下中の各キーの振る舞いを決める関数
+void event_modifier_for_muhenkan_pressed(struct input_event *ev)
+{
+    switch (ev->code)
+    {
+    case KEY_O:
+        ev->code = KEY_BACKSPACE;
+        break;
+    default:
+        break;
+    }
+}
+
 void event_modifier_in_loop(struct input_event *ev)
 {
     if (ev->code == KEY_MUHENKAN)
     {
+        /* 無変換 */
+        // 保持している無変換キーの状態を更新する
+        g_value_muhenkan = ev->value;
         // 無変換単体は無効化する。code 0をwriteしても無視されるだけで害はない。
         ev->code = 0;
         return;
     }
-    if (ev->code == KEY_I)
+    if (0 < g_value_muhenkan)
     {
-        ev->code = KEY_O;
-    }
-    else if (ev->code == KEY_O)
-    {
-        ev->code = KEY_A;
+        event_modifier_for_muhenkan_pressed(ev);
     }
 }
