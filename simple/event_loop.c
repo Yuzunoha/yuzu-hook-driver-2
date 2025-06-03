d0fb824dc7f7c7a42c42dc3c04515aa6479bba89
Yuzunoha <studysteadystandby@gmail.com>
2025-06-03 18:22:37 +0900

    // 無変換単体は無効化する。code 0をwriteしても無視されるだけで害はない。

--- simple/event_loop.c
+++ simple/event_loop.c
@@ -3,6 +3,12 @@
 
 void event_modifier_in_loop(struct input_event *ev)
 {
+    if (ev->code == KEY_MUHENKAN)
+    {
+        // 無変換単体は無効化する。code 0をwriteしても無視されるだけで害はない。
+        ev->code = 0;
+        return;
+    }
     if (ev->code == KEY_I)
     {
         ev->code = KEY_O;
