06b5c7e427661ccab76181cc3dfd5aeba3ecc2ac
Yuzunoha <studysteadystandby@gmail.com>
2025-06-04 19:08:38 +0900

    リネーム

--- main/search_keyboard_event_path.c
+++ main/search_keyboard_event_path.c
@@ -67,7 +67,7 @@ int this_device_path_is_keyboard(char *device_path)
   return result;
 }
 
-int get_keyboard_event()
+void search_keyboard_event_path(char *arg)
 {
   char path[32];
   for (int i = 0; i <= 30; i++)
